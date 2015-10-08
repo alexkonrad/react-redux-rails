9313fc64f5cf64e45e27a2d670dc48dae4f4ea70
Alex Konrad <alexkonrad08@gmail.com>
2015-10-08 16:40:17 -0400

    first commit

---
+++ lib/react/redux/rails.rb
@@ -0,0 +1,9 @@
+require "react/redux/rails/version"
+
+module React
+  module Redux
+    module Rails
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/react/redux/rails/version.rb
@@ -0,0 +1,7 @@
+module React
+  module Redux
+    module Rails
+      VERSION = "0.1.0"
+    end
+  end
+end
